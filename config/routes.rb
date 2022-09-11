334fd87df724e0902f6411c91876968a30e2c779
OpheNg <ophe.ng@live.fr>
2022-09-11 23:01:21 +0200

    signin signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#dashboard'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'dashboard', to: 'pages#dashboard'
+  get 'home', to: 'pages#home'
 
   resources :courses
   resources :student_courses
