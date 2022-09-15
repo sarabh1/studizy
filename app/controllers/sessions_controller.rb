e0aa1682581d6626057a101b773c8e0dea5b9a3b
OpheNg <ophe.ng@live.fr>
2022-09-15 18:44:46 +0200

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
-  def index
-    @sessions = Session.all
-  end
+  # def index
+  #   @sessions = Session.all
+  # end
   # def show
   #   @sessions=Session.all
   # end
+  def index
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @sessions = Session.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
 
 end
