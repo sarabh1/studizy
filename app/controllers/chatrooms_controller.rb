67d014b5d6697a5c6d9071d57b470e1a8332321c
sara bahhar <bahhar.sara95@gmail.com>
2022-09-17 11:21:07 +0200

    ok

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -7,6 +7,5 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    @messages = Message.where(chatroom_id: @chatroom.id)
   end
 end
