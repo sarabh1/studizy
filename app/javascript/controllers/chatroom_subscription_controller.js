8c46ea0a2e7fda2b5e685bf8de6fce8bf005410e
OpheNg <ophe.ng@live.fr>
2022-09-17 12:18:51 +0200

    stimulus OK - mais bug : les mess s insère sous linput, au lieu de au dessus... quand on rafraichit ca se remet en place mais c chelou

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -11,10 +11,13 @@ export default class extends Controller {
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      {received: data => this.#insertMessageAndScrollDown(data)},
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
-
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
 }
