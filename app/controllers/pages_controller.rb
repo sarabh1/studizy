073651e42043d90396cbbdda587db1e900979776
OpheNg <ophe.ng@live.fr>
2022-09-17 04:16:26 +0200

    render events ok , mais ca affiche session description, c 'est Ok faudra tager les cours par couleur :) - seed modifiée pour les dates -- Problème de link_to, j'arrive pas à linker l'event dans l'agenda vers course show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,11 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @user = current_user
     @sessions = current_user.sessions
-   
+    @courses = current_user.courses
+
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @sessions = Session.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
-
-
-
 end
