a04f7ec8bbef6dce839407be2d77a11794fcfb34
OpheNg <ophe.ng@live.fr>
2022-09-17 11:31:06 +0200

    setup stimulus jusqu'à suscribe INCLUS

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -7,6 +7,6 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    @messages = Message.where(chatroom_id: @chatroom.id)
+    # @messages = Message.where(chatroom_id: @chatroom.id)
   end
 end
