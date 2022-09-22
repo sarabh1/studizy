2f85932706446c93893f89a5cd73afaef78f5e72
OpheNg <ophe.ng@live.fr>
2022-09-22 23:03:12 +0200

    deadlines OK - pas besoin de act as taggable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,9 @@ class PagesController < ApplicationController
     @sessions = Session.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     @sessions_fourth = @sessions.select {|session| session.start_date > DateTime.now}.sort_by(&:start_time).first(11)
 
-    @red_sessions = @sessions_fourth.select {|session| session.start_date <= DateTime.now + 2}
-    @yellow_sessions = @sessions_fourth.select {|session| session.start_date > DateTime.now + 3 && session.start_date < DateTime.now + 6}
-    @green_sessions = @sessions_fourth.select {|session| session.start_date > DateTime.now + 6}
+    @red_sessions = @sessions_fourth.select {|session| (session.name.include?("TD") || session.name.include?("Exam") || session.name.include?("Homework")) && session.start_date <= DateTime.now + 2}
+    @yellow_sessions = @sessions_fourth.select {|session| (session.name.include?("TD") || session.name.include?("Exam") || session.name.include?("Homework")) && session.start_date > DateTime.now + 3 && session.start_date < DateTime.now + 6}
+    @green_sessions = @sessions_fourth.select {|session| (session.name.include?("TD") || session.name.include?("Exam") || session.name.include?("Homework")) && session.start_date > DateTime.now + 6}
   end
 
   def chatroom
