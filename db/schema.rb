a81fc6ba29a2c741ec528dc53e6a3cc9c2dce350
OpheNg <ophe.ng@live.fr>
2022-09-27 12:14:07 +0200

    ajout colonne average a courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_105424) do
+ActiveRecord::Schema.define(version: 2022_09_27_100855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_09_26_105424) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "average"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
