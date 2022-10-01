df2be65435004ccdcfa0ba47a7b9a1e88b188b0b
OpheNg <ophe.ng@live.fr>
2022-10-01 21:35:19 +0200

    home

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
-
 require "open-uri"
 
 Message.destroy_all
