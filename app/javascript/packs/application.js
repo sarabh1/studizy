7ee6ac8aec74fc1b3c9066bafa52b244c6567ab7
OpheNg <ophe.ng@live.fr>
2022-10-01 12:24:46 +0200

    ok JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,30 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+// import "controllers"
+// import "bootstrap"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "controllers"
-import "bootstrap"
