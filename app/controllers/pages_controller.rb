5d91ba62c9c15998cff375e1b9e4701d533eeb6c
OpheNg <ophe.ng@live.fr>
2022-10-05 22:56:41 +0200

    changement font partout OPEN SANS _ ignon ecriture arrondie

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -38,11 +38,18 @@ class PagesController < ApplicationController
     @messages = Message.left_joins(:attachments_attachments).where.not(active_storage_attachments: { id: nil })
   end
 
+  # private
+
+  # def date_range
+  #   beginning = start_date.beginning_of_week
+  #   ending    = start_date.end_of_week - 2.day
+  #   (beginning..ending).to_a
+  # end
   private
 
   def date_range
-    beginning = start_date.beginning_of_week
-    ending    = start_date.end_of_week - 2.day
+    beginning = start_date.beginning_of_week + 1.day
+    ending    = start_date.end_of_week - 1.day
     (beginning..ending).to_a
   end
 
